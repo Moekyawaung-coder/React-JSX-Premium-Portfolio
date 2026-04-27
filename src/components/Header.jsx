3da40334dfd2ecd9aa5cc4c45490482d2684db17
Moekyawunng <moekyawaung@diplomats.com>
2026-04-28 01:36:43 +0630

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Moe Kyaw Aung</h1>
+      <nav>
+        <a href="#hero">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
