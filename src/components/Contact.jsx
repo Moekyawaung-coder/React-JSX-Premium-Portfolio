fc461ff1f0d30d3342192875021a09ccb54dd908
Moekyawunng <moekyawaung@diplomats.com>
2026-04-28 01:42:43 +0630

    Add Contact component with contact information

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: <a href="mailto:moekyaw@engineer.com">moekyaw@engineer.com</a></p>
+      <p>Phone: +959889000889</p>
+      <p>GitHub: <a href="https://github.com/Moe-KyawAung">Moe-KyawAung</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/moe-kyaw-aung-2653093a1/">Moe Kyaw Aung</a></p>
+    </section>
+  );
+}
